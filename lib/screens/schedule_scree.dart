4e9ea6712313ca4a3d329e391bc32f508c81956a
sanel306 <erickshayo306@gmail.com>
2023-05-04 21:32:50 +0300

    working with the schedule screen

--- lib/screens/schedule_scree.dart
+++ lib/screens/schedule_scree.dart
@@ -6,14 +6,12 @@ class ScheduleScreen extends StatefulWidget {
 }
 
 class _ScheduleScreenState extends State<ScheduleScreen> {
-
   int _buttonIndex = 0;
 
   final _scheduleWidgets = [
     Container(),
     Container(),
     Container(),
-
   ];
   @override
   Widget build(BuildContext context) {
@@ -31,6 +29,39 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
                   fontSize: 32,
                   fontWeight: FontWeight.w500,
                 ),
+              ),
+            ),
+            SizedBox(height: 12),
+            Container(
+              padding: EdgeInsets.all(15),
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: Color(0xFFF4F6FA),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 25),
+                      decoration: BoxDecoration(
+                        // color: Color(value)
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Upcoming",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          // color: 
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             )
           ],
