6e147cb0f9dbe6bc202a7fd9f2c70d956a755fe2
Erick Shayo <erickshayo306@gmail.com>
2023-06-05 07:49:12 +0300

    first commit

--- lib/screens/clinic_visit_screen.dart
+++ lib/screens/clinic_visit_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
-class ClinicVisit extends StatelessWidget {
+class ClinicVisit extends StatefulWidget {
   const ClinicVisit({super.key});
 
+  @override
+  State<ClinicVisit> createState() => _ClinicVisitState();
+}
+
+class _ClinicVisitState extends State<ClinicVisit> {
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,76 +19,75 @@ class ClinicVisit extends StatelessWidget {
         title: Text("Clinic Visit Appointment"),
         centerTitle: true,
       ),
-      
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
         child: ListView(
-            children: [
-              TextField(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Full Name",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.person),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Email Address",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Age",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.auto_graph_rounded),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Gender",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(FontAwesomeIcons.genderless),
+              ),
+            ),
+            SizedBox(height: 20),
+            InkWell(
+              onTap: () {},
+              child: TextField(
                 decoration: InputDecoration(
-                  labelText: "Full Name",
+                  labelText: "Date",
                   border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.person),
+                  prefixIcon: Icon(Icons.calendar_month_outlined),
                 ),
               ),
-              SizedBox(height: 20),
-              TextField(
+            ),
+            SizedBox(height: 20),
+            InkWell(
+              onTap: () {},
+              child: TextField(
                 decoration: InputDecoration(
-                  labelText: "Email Address",
+                  labelText: "Time",
                   border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.email),
+                  prefixIcon: Icon(FontAwesomeIcons.clock),
                 ),
               ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Age",
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.auto_graph_rounded),
-                ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              minLines: 6,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: InputDecoration(
+                labelText: "Enter your symptoms here",
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Gender",
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(FontAwesomeIcons.genderless),
-                ),
-              ),
-              SizedBox(height: 20),
-              InkWell(
-                onTap: () {},
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Date",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.calendar_month_outlined),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              InkWell(
-                onTap: () {},
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Time",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(FontAwesomeIcons.clock),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                minLines: 6,
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(
-                  labelText: "Enter your symptoms here",
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: Container(
         padding: EdgeInsets.all(15),
